6add251100cff0bc318dbedb49e86282e3b6bba3
Shi Lei <shi_lei@massclouds.com>
2024-05-16 10:13:48 +0000

    add some contents

--- hal_base/src/lib.rs
+++ hal_base/src/lib.rs
@@ -1,11 +1,17 @@
 #![cfg_attr(not(target_os = "linux"), no_std)]
 #![feature(exclusive_wrapper)]
+#![feature(naked_functions)]
 
 mod platform;
 pub use platform::terminate;
 
 mod time;
 pub use time::current_time;
+
+pub mod arch;
+pub mod cpu;
+pub mod trap;
+pub mod paging;
 
 pub fn init() {
 }
